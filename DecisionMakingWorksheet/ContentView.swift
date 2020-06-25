726e80b5dc770d1dbc782fb2e459d0c6a248f847
delorali <delorazli@gmail.com>
2020-06-24 18:02:55 -0700

    Initial Commit

---
+++ DecisionMakingWorksheet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DecisionMakingWorksheet
+//
+//  Created by Delora Li on 6/24/20.
+//  Copyright © 2020 Delora Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
